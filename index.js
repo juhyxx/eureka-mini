c787a2041e41d3629458eecdcc8decf86d249b9c
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2018-04-16 23:22:57 +0200

    Support for loading images

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const handlebars = require("handlebars");
 const less = require("less");
 const colors = require("colors");
 const watch = require("watch");
+const WebSocketServer = require("websocket").server;
+const url = require("url");
 
 const PORT = 8080;
 const PUBLIC_DIR = "public";
@@ -17,12 +19,19 @@ if (!fs.existsSync(PUBLIC_DIR)) {
 }
 
 let requestListener = function(req, res) {
-  console.log(colors.green(req.method), req.url);
-
   let content;
 
-  switch (req.url) {
-    case "/":
+  let url = req.url;
+
+  switch (true) {
+    case /\/app/.test(url):
+      res.writeHead(200, { "Content-Type": "image/jpeg;" });
+      content = fs.readFileSync(url.replace(/\//, ""));
+      res.end(content);
+      break;
+
+    case /^\/$/.test(url):
+      console.log(colors.green(req.method), req.url);
       http.get(`${apiUrl}/categories/`, response => {
         let requestData = "";
 
@@ -35,11 +44,16 @@ let requestListener = function(req, res) {
       });
       res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
       break;
-    case "/style.css":
+
+    case /^\/style.css/.test(url):
+      console.log(colors.green(req.method), req.url);
       res.writeHead(200, { "Content-Type": "text/css; charset=utf-8" });
       content = fs.readFileSync("public/style.css", "utf8");
       res.end(content);
+      break;
+
     default:
+      console.log(colors.red(req.method), req.url);
       res.writeHead(404);
       res.end();
   }
@@ -50,6 +64,22 @@ server.listen(PORT, () => {
   console.log(`listening ${PORT} ...`.green);
 });
 
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+wsServer.on("request", function(r) {
+  wsConnection = r.accept("echo-protocol", r.origin);
+});
+
+function reload() {
+  if (wsConnection) {
+    wsConnection.sendUTF("reload");
+  } else {
+    console.log("Client is not connected".red);
+  }
+}
+
 watch.watchTree("app", { ignoreDotFiles: true }, (f, prev, curr) => {
   if (typeof f == "object" && prev === null && curr === null) {
   } else {
@@ -57,9 +87,11 @@ watch.watchTree("app", { ignoreDotFiles: true }, (f, prev, curr) => {
     switch (path.extname(f)) {
       case ".hbs":
         template = compileTemplate();
+        reload();
         break;
       case ".less":
         compileLess();
+        reload();
         break;
     }
   }
