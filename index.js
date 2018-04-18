7ab329366a4cdc33ed84f6e9044e38f4cffdc0ee
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2018-04-18 08:48:02 +0200

    Rendering refactor

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const handlebars = require('handlebars');
+
 const less = require('less');
 const colors = require('colors');
 const watch = require('watch');
 const url = require('url');
-const server = require('./server');
+const server = require('./lib/server');
+const renderer = require('./lib/renderer');
+const common = require('./lib/common');
+const config = require('./lib/config.json');
 
-const PUBLIC_DIR = 'public';
-const SOURCE_DIR = 'app';
-const API_URL = 'http://python-servers-vtnovk529892.codeanyapp.com:5000';
-
-function mkdir(dir = '') {
-  if (!fs.existsSync(PUBLIC_DIR + dir)) {
-    fs.mkdirSync(PUBLIC_DIR + dir);
-  }
-}
-
-mkdir();
-handlebars.registerPartial('header', fs.readFileSync('app/partials/header.hbs', 'utf8'));
-handlebars.registerPartial('footer', fs.readFileSync('app/partials/footer.hbs', 'utf8'));
-handlebars.registerPartial('reload', fs.readFileSync('app/partials/reload.hbs', 'utf8'));
-handlebars.registerHelper('eq', (a, b) => a === b);
-handlebars.registerHelper('currency', a =>
-  (a || '')
-    .toString()
-    .split('')
-    .reverse()
-    .join('')
-    .split(/(.{3})/)
-    .join(' ')
-    .split('')
-    .reverse()
-    .join('')
-    .trim()
-);
+common.mkdir();
+renderer.init();
 
 watch.watchTree('app', { ignoreDotFiles: true }, (f, prev, curr) => {
   if (typeof f == 'object' && prev === null && curr === null) {
@@ -45,7 +22,7 @@ watch.watchTree('app', { ignoreDotFiles: true }, (f, prev, curr) => {
 
     switch (path.extname(f)) {
       case '.hbs':
-        render(f).then(() => server.reload());
+        renderer.render().then(() => server.reload());
         break;
 
       case '.less':
@@ -57,82 +34,19 @@ watch.watchTree('app', { ignoreDotFiles: true }, (f, prev, curr) => {
 
 function compileLess() {
   return new Promise(resolve => {
-    try {
-      fs.unlinkSync('public/style.css');
-    } catch (e) {
-      console.log('unable to delete style.css'.yellow);
-    }
-    fs.readFile('app/styles/index.less', 'utf8', (err, content) => {
-      less.render(content).then(renderedContent => {
-        fs.writeFile(path.format({ dir: PUBLIC_DIR, name: 'style.css' }), renderedContent.css, 'utf8', () => resolve());
-      });
-    });
-  });
-}
-
-function loadApiData(url) {
-  return new Promise((resolve, reject) => {
-    http.get(`${API_URL}/${url}`, response => {
-      const { statusCode } = response;
-      let requestData = '';
-      if (statusCode !== 200) {
-        reject(statusCode);
+    //solves problem with LESS caching of imported files
+    const fileManagers = (less.environment && less.environment.fileManagers) || [];
+    fileManagers.forEach(fileManager => {
+      if (fileManager.contents) {
+        fileManager.contents = {};
       }
-
-      response.on('data', chunk => (requestData += chunk));
-      response.on('end', () => {
-        resolve(JSON.parse(requestData));
+    });
+    fs.readFile('app/styles/index.less', 'utf8', (err, content) => {
+      less.render(content).then(rendered => {
+        fs.writeFile(common.public('style.css'), rendered.css, 'utf8', () => resolve());
       });
     });
   });
 }
 
-function render(file) {
-  return new Promise(resolve => {
-    console.log(`Rendering...`.yellow);
-
-    const templates = {
-      index: handlebars.compile(fs.readFileSync(path.format({ dir: SOURCE_DIR, name: 'index.hbs' }), 'utf8')),
-      page404: handlebars.compile(fs.readFileSync(path.format({ dir: SOURCE_DIR, name: '404.hbs' }), 'utf8')),
-      category: handlebars.compile(fs.readFileSync(path.format({ dir: SOURCE_DIR, name: 'category.hbs' }), 'utf8')),
-      detail: handlebars.compile(fs.readFileSync(path.format({ dir: SOURCE_DIR, name: 'detail.hbs' }), 'utf8'))
-    };
-    fs.writeFile('public/404.html', templates.page404(), 'utf8');
-
-    loadApiData('categories/').then(categories => {
-      let result = templates.index({ categories: categories });
-
-      fs.writeFile(path.format({ dir: PUBLIC_DIR, name: 'index.html' }), result, 'utf8');
-      mkdir('/category');
-      categories.forEach(category => {
-        mkdir(`/category/${category.categoryId}`);
-        loadApiData(`products/${category.categoryId}/0/100/`).then(products => {
-          products.forEach(product => {
-            loadApiData(`offers/${product.productId}/0/100/`).then(offers => {
-              offers = offers || [];
-
-              let prices = offers.map(offer => offer.price);
-              product.rangeMin = Math.min.apply(Math, prices);
-              product.rangeMax = Math.max.apply(Math, prices);
-              product.description = (offers.find(offer => !!offer.description) || {}).description;
-              product.category = category;
-
-              result = templates.category({ selected: category, categories: categories, products: products });
-              fs.writeFile(`public/category/${category.categoryId}/index.html`, result, 'utf8');
-
-              result = templates.detail({ selected: product, offers: offers.sort((a,b)=>a.price-b.price) });
-              fs.writeFile(`public/category/${category.categoryId}/${product.productId}.html`, result, 'utf8');
-            });
-          });
-        });
-			});
-			setTimeout(() => {
-        resolve();
-      }, 1500);
-    });
-  });
-}
-
-Promise.all([render(), compileLess()]).then(() => {
-  server.run();
-});
+Promise.all([renderer.render(), compileLess()]).then(() => server.run());
