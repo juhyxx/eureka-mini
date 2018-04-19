ce84531f0f6c41c5447b88151f820751e43430ad
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2018-04-19 07:22:51 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
 const less = require('less');
 const colors = require('colors');
 const watch = require('watch');
-const url = require('url');
+
 const server = require('./lib/server');
 const renderer = require('./lib/renderer');
 const common = require('./lib/common');
 const config = require('./lib/config.json');
 
-common.mkdir();
-renderer.init();
 
-watch.watchTree('app', { ignoreDotFiles: true }, (f, prev, curr) => {
-  if (typeof f == 'object' && prev === null && curr === null) {
-    console.log('Watching'.blue);
-  } else {
-    console.log('change'.blue, f);
-
-    switch (path.extname(f)) {
-      case '.hbs':
-        renderer.render().then(() => server.reload());
-        break;
-
-      case '.less':
-        compileLess().then(() => server.reload());
-        break;
-      case '.js':
-				 fs.writeFileSync(common.public('script.js'), fs.readFileSync(common.source('script.js'), 'utf8'), 'utf8');
-				 server.reload();
-        break;
-    }
-  }
-});
 
 function compileLess() {
   return new Promise(resolve => {
@@ -53,4 +28,30 @@ function compileLess() {
   });
 }
 
-Promise.all([renderer.render(), compileLess()]).then(() => server.run());
+common.mkdir();
+renderer.init();
+
+Promise.all([renderer.render(), compileLess()]).then(() => {
+  server.run();
+  watch.watchTree('app', { ignoreDotFiles: true }, (f, prev, curr) => {
+    if (typeof f == 'object' && prev === null && curr === null) {
+      console.log('Watching for changes...'.blue);
+    } else {
+      console.log('change'.blue, f);
+
+      switch (path.extname(f)) {
+        case '.hbs':
+          renderer.render().then(() => server.reload());
+          break;
+
+        case '.less':
+          compileLess().then(() => server.reload());
+          break;
+        case '.js':
+          fs.writeFileSync(common.public('script.js'), fs.readFileSync(common.source('script.js'), 'utf8'), 'utf8');
+          server.reload();
+          break;
+      }
+    }
+  });
+});
