8320df59ff0c3f3958bbf0452ed02f5890e19459
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2018-04-19 12:18:07 +0200

    Added review

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # eureka-mini
+
+##How to run
+```yarn && yarn start```
+or
+```npm run && npm start```
+
+## Used technologies
+* **Node** as rendering engine and server
+* **Handlebars** for HTML templating
+* **LESS** for generation of CSS
+* **WebSockets** for reloading on change
+
+## How it works
+After run:
+1. Node loads all required data from API
+1. Node renders and compiles into public folder (see config.json)
+	* HTML files (server side rendering), compiles
+	* SASS
+	* JavaScript
+2. Web server is started on defined port (see config.json)
+3. Watcher is started (shouldn't be included into distribution version)
+4. Ready for using, happy browsing!
+
+*In real traffic should be solved starting of api data load and rendering periodically, or on same event or on demand.*
--- app/script.js
+++ app/script.js
@@ -4,11 +4,120 @@ window.addEventListener(
     document.querySelector('a.expand').addEventListener('click', function() {
       var el = document.querySelector('div.prices');
       el.className = el.className.replace('collapsed', '');
-		});
-
-		document.querySelector('.gallery .thumbnails .image').addEventListener('click', function() {
-     console.log('click')
     });
+
+    var collapsible = document.querySelector('.collapsible');
+
+    if (collapsible.querySelector('.content').clientHeight < 50) {
+      collapsible.className = collapsible.className.replace('collapsed', '');
+    }
+
+    document.querySelector('.collapsible .expand').addEventListener('click', function(e) {
+      var el = e.target.parentNode;
+      el.className = el.className.replace('collapsed', '');
+    });
+
+    if (document.querySelector('.thumbnails')) {
+      document.querySelector('.gallery .show').addEventListener('click', function(e) {
+        e.stopPropagation();
+        showGallery();
+      });
+      document.querySelector('.gallery .thumbnails .image').addEventListener('click', function(e) {
+        e.stopPropagation();
+        showGallery(e.target);
+      });
+    }
   },
   true
 );
+
+function Images(urlList) {
+  this.selected = 0;
+  this.urlList = urlList;
+}
+
+Images.prototype.next = function() {
+  this.selected++;
+  if (this.selected >= this.urlList.length) {
+    this.selected = 0;
+  }
+  return this.get();
+};
+
+Images.prototype.prev = function() {
+  this.selected--;
+  if (this.selected < 0) {
+    this.selected = this.urlList.length - 1;
+  }
+  return this.get();
+};
+
+Images.prototype.get = function() {
+  return this.urlList[this.selected];
+};
+
+Images.prototype.setPointer = function(url) {
+  this.selected = this.urlList.indexOf(url);
+};
+
+function showGallery(target) {
+  imagesList = new Images(
+    [].slice.call(document.querySelectorAll('.image')).map(function(img) {
+      return img.style.backgroundImage;
+    })
+  );
+  if (target) {
+    imagesList.setPointer(target.style.backgroundImage);
+  }
+
+  var win = document.createElement('div');
+  win.id = 'preview';
+  document.body.appendChild(win);
+
+  var toolbar = document.createElement('div');
+  toolbar.id = 'toolbar';
+  win.appendChild(toolbar);
+
+  var container = document.createElement('div');
+  container.id = 'container';
+  win.appendChild(container);
+
+  var close = document.createElement('div');
+  close.id = 'close';
+  close.innerHTML = '×';
+  toolbar.appendChild(close);
+
+  var left = document.createElement('div');
+  left.id = 'left';
+  left.innerHTML = '⮜';
+  container.appendChild(left);
+
+  var img = document.createElement('div');
+  img.id = 'image';
+  img.style.backgroundImage = imagesList.get();
+  container.appendChild(img);
+
+  var right = document.createElement('div');
+  right.id = 'right';
+  right.innerHTML = '⮞';
+  container.appendChild(right);
+
+  function closeHandler(e) {
+    document.body.removeChild(win);
+    document.body.removeEventListener('click', closeHandler);
+  }
+
+  close.addEventListener('click', closeHandler);
+  document.body.addEventListener('click', closeHandler);
+  win.addEventListener('click', function(e) {
+    e.stopPropagation();
+  });
+  left.addEventListener('click', function(e) {
+    e.stopPropagation();
+    img.style.backgroundImage = imagesList.prev();
+  });
+  right.addEventListener('click', function(e) {
+    e.stopPropagation();
+    img.style.backgroundImage = imagesList.next();
+  });
+}
